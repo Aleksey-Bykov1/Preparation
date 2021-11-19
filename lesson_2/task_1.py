d6a2ac7e2f6aecb3b4db67fc6f829164688a68e3
Alexey <max_89@inbox.ru>
2021-11-19 08:15:52 +0300

    task_1_2

---
+++ lesson_2/task_1.py
@@ -0,0 +1,23 @@
+"""
+1. Проверить механизм наследования в Python. Для этого создать два класса. Первый — родительский (ItemDiscount),
+должен содержать статическую информацию о товаре: название и цену. Второй — дочерний (ItemDiscountReport),
+должен содержать функцию (get_parent_data), отвечающую за отображение информации о товаре в одной строке.
+Проверить работу программы, создав экземпляр (объект) родительского класса.
+"""
+
+
+class ItemDiscount:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class ItemDiscountReport(ItemDiscount):
+    def get_parent_data(self):
+        print(f'name - {self.name},price - {self.price}')
+
+
+if __name__ == '__main__':
+    product = ItemDiscount('printer', '700')
+    product_report = ItemDiscountReport(product.name, product.price)
+    product_report.get_parent_data()
